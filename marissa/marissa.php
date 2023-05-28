d18fd34004cf2ea966090b09d96675da9da2d31a
mariisamagasin <134884280+mariisamagasin@users.noreply.github.com>
2023-05-29 01:41:42 +0200

    Add files via upload

---
+++ marissa/marissa.php
@@ -0,0 +1,9 @@
+<?PHP
+$ordre1=$-post[ ordre1]
+$ordre2=$-post[ ordre2]
+$ordre3=$-post[ ordre3]
+$ordre4=$-post[ ordre4]
+$ordre5=$-post[ ordre5]
+
+echo $ordre1 $ordre2 $ordre3 $ordre4 $ordre5;
+?>
